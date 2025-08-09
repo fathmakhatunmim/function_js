99e1b7bf6389f25b819d2a3ed000b5d93f8760c0
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-08-09 12:54:50 +0600

    funtion basic

---
+++ function.js
@@ -0,0 +1,26 @@
+const price = 45;
+
+//function declare
+function fanoffkor( )
+{
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off the fan');
+
+}
+
+//call the function
+fanoffkor( );
+
+
+//function name()
+//}
+
+function brushteeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min brush koro');
+}
+//call the function
+
+brushteeth();
