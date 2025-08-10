a2270188ed9098fbaa79f4772ce896596125267b
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-08-10 08:44:48 +0600

    funtion

---
+++ argument.js
@@ -0,0 +1,21 @@
+function doubleIt(number){
+    const doubled = number * 2;
+    console.log(number,doubled);
+}
+
+console. log('i will call the function')
+doubleIt(15);
+console.log('---------------');
+doubleIt(88);
+console.log('---------------');
+doubleIt(873);
+
+
+function diff(num1,num2){
+    const diff =num1- num2;
+    console.log(num1,num2,'difference is ' ,diff);
+}
+const fatherAge = 40;
+const myAge = 10;
+diff(fatherAge, myAge);
+
---
+++ parameter.js
@@ -0,0 +1,15 @@
+function vaatKhao()
+{
+              console.log('hand wash kore aso');
+              console.log('boso');
+              console.log('plate a khabar dhabar nao');
+              console.log('dere sunsto khauya');
+}
+vaatKhao();
+
+function square(number){
+     
+    console.log(number);
+    console.log('value of the number parameter',number);
+}
+square(4);
